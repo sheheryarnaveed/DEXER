7f052de75110dc078856ed83376173a1d9a7e063
sheheryarnaveed <34706398+sheheryarnaveed@users.noreply.github.com>
2018-03-24 14:45:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ tableSvc.createTableIfNotExists('ScoreTable', function(error, result, response){
   }
 });
 
-// Setup Restify Server
+// Settin up Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
 console.log('%s listening to %s', server.name, server.url);
